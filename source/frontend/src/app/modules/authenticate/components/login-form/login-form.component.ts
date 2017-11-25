f8cb32f79a4ec102ec14674f26f078af18c31e57
toanleviet95 <toanleviet95@gmail.com>
2017-11-25 17:02:43 +0700

    Fix Login & module Newsfeed

--- source/frontend/src/app/modules/authenticate/components/login-form/login-form.component.ts
+++ source/frontend/src/app/modules/authenticate/components/login-form/login-form.component.ts
@@ -8,11 +8,52 @@ import { LoginService } from '../../services/login.service';
 })
 
 export class LoginFormComponent implements OnInit {
+    user = {
+      email: '',
+      password: ''
+    };
+
+    emailErr = '';
+    passwordErr = '';
+
     ngOnInit() {
 
     }
 
+    validateEmail(email) {
+      const re = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+      return re.test(email);
+    }
+
     onSignin() {
+      this.emailErr = '';
+      if (!this.user.email || !this.validateEmail(this.user.email)) {
+        this.emailErr = 'Email is not valid';
+      }
+      this.passwordErr = '';
+      if (!this.user.password) {
+        this.passwordErr = 'Password is not valid';
+      }
+
+      if (this.emailErr || this.passwordErr) {
+        return;
+      }
+
+      if (this.user.email === 'user@gmail.com' && this.user.password === '123456') {
+
+      } else if (this.user.email === 'staff@gmail.com' && this.user.password === '123456') {
+
+      } else if (this.user.email === 'leader@gmail.com' && this.user.password === '123456') {
+
+      } else {
+        this.emailErr = 'Email/Password is not correct';
+        this.passwordErr = '';
+        return;
+      }
+
+      this.passwordErr = '';
+      this.emailErr = '';
+
       window.location.href = 'newsfeed';
     }
 }
--- source/frontend/src/app/modules/newsfeed/components/newsfeed-noti-bar/newsfeed-noti-bar.component.ts
+++ source/frontend/src/app/modules/newsfeed/components/newsfeed-noti-bar/newsfeed-noti-bar.component.ts
@@ -6,7 +6,45 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class NewsfeedNotiBarComponent implements OnInit {
-    ngOnInit() {
+    arrContent = [];
+    content = { header: '', content: '' };
+    idx = 0;
 
+    ngOnInit() {
+      this.arrContent = [
+        {
+          header: 'Thông báo (Lần 1) V/v: Thực hiện nội quy quản lý sử dụng Nhà Chung cư ...',
+          content: 'Bản Nội quy này quy định các nội dung về quản lý sử dụng cụm nhà chung cư Him Lam Chợ Lớn ...'
+        },
+        {
+          header: 'Thông báo (Lần 2) V/v: Thực hiện nội quy quản lý sử dụng Nhà Chung cư ...',
+          content: 'Bản Nội quy này quy định các nội dung về quản lý sử dụng cụm nhà chung cư Him Lam Chợ Lớn ...'
+        },
+        {
+          header: 'Thông báo (Lần 3) V/v: Thực hiện nội quy quản lý sử dụng Nhà Chung cư ...',
+          content: 'Bản Nội quy này quy định các nội dung về quản lý sử dụng cụm nhà chung cư Him Lam Chợ Lớn ...'
+        },
+        {
+          header: 'Thông báo (Lần 4) V/v: Thực hiện nội quy quản lý sử dụng Nhà Chung cư ...',
+          content: 'Bản Nội quy này quy định các nội dung về quản lý sử dụng cụm nhà chung cư Him Lam Chợ Lớn ...'
+        }
+      ];
+      this.content = this.arrContent[0];
+    }
+
+    onNext() {
+      this.idx++;
+      if (this.idx === this.arrContent.length) {
+        this.idx = 0;
+      }
+      this.content = this.arrContent[this.idx];
+    }
+
+    onPrev() {
+      this.idx--;
+      if (this.idx === -1) {
+        this.idx = this.arrContent.length;
+      }
+      this.content = this.arrContent[this.idx];
     }
 }
