e970f31513075b6f52a90878cdee183e50021ac2
dungnguyen <dungtiennguyen1995@gmail.com>
2017-11-25 20:58:26 +0700

    update staff manage

---
+++ source/frontend/src/app/modules/complain/components/complain/complain.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component ({
+    selector: 'complain',
+    templateUrl: './complain.component.html'
+})
+
+export class ComplainComponent implements OnInit {
+    ngOnInit() {
+
+    }
+}
