a17d4f46df97f6af87fd479fac8f65d84a9cf80c
toanleviet95 <toanleviet95@gmail.com>
2017-11-25 19:14:44 +0700

    Fix router link

---
+++ source/frontend/src/app/shared/components/left-side-bar/left-side-bar.module.ts
@@ -0,0 +1,17 @@
+
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+    imports: [
+      RouterModule
+    ],
+    declarations: [
+
+    ],
+    providers: [
+
+    ]
+})
+
+export class LeftSideBarModule { }
