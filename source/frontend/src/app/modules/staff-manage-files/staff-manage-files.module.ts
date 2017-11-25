f14de7b26f6098db9119218d0123f9e860b599cd
DuyQuangNgoDinh <ngodinhduyquang@gmail.com>
2017-11-25 22:22:11 +0700

    Fix dropdown

--- source/frontend/src/app/modules/staff-manage-files/staff-manage-files.module.ts
+++ source/frontend/src/app/modules/staff-manage-files/staff-manage-files.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { StaffManageFilesRoutingModule } from './staff-manage-files.routing';
 
 import { StaffManageFilesContentComponent } from './components/staff-manage-files-content/staff-manage-files-content.component';
 import { LeftSideBarComponent } from '../../shared/components/left-side-bar/left-side-bar.component';
@@ -9,7 +10,8 @@ import { LeftSideBarComponent } from '../../shared/components/left-side-bar/left
 @NgModule({
     imports: [
         CommonModule,
-        FormsModule
+        FormsModule,
+        StaffManageFilesRoutingModule
     ],
     declarations: [
         LeftSideBarComponent,
